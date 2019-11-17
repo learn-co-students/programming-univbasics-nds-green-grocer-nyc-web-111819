49c8c4fad9d5cc9e1e6c930f256c573fbb0f69bc
Natalie Galligan <nat.galligan@gmail.com>
2019-11-17 01:27:55 +0000

    Done.

--- README.md
+++ README.md
@@ -107,7 +107,7 @@ create.
   * `Array`: a collection of items to search through
 * Returns:
   * `nil` if no match is found
-  * the matching `Hash` if a match is found between the desired name and a given 
+  * the matching `Hash` if a match is found between the desired name and a given
     `Hash`'s :item key
 
 ### Write the `consolidate_cart` Method
@@ -235,8 +235,8 @@ it should update the cart with clearance applied to PEANUT BUTTER and SOY MILK:
 ]
 ```
 
-The `Float` class' built-in [round][round] method will be helpful here to make 
-sure your values align. `2.4900923090909029304` becomes `2.5` if we use it 
+The `Float` class' built-in [round][round] method will be helpful here to make
+sure your values align. `2.4900923090909029304` becomes `2.5` if we use it
 like so: `2.4900923090909029304.round(2)`
 
 ### Write the `checkout` Method
--- grocer.rb
+++ grocer.rb
@@ -1,27 +1,90 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+
+collection.each {|cart_item|
+  if cart_item[:item] == name
+    return cart_item
+  end
+}
+nil
 end
+
+def consolidation_method(uniq_product, new_cart)
+    cart_item_name = uniq_product[:item]
+
+    index = 0
+    while index < new_cart.count do
+    if cart_item_name == new_cart[index][:item]
+      return new_cart[index]
+    end
+
+    index += 1
+    end
+  new_cart << uniq_product
+  uniq_product[:count] = 0
+  uniq_product
+end
+
 
 def consolidate_cart(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  index = 0
+  new_cart_array = []
+
+  while index < cart.count do
+    cart_item_name = cart[index][:item]
+    cart_item = cart[index]
+
+    new_cart_hash = consolidation_method(cart_item,new_cart_array)
+
+    new_cart_hash[:count] += 1
+    index += 1
+
+  end
+
+  new_cart_array
 end
 
-def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+def apply_coupon_to_cart(consolidated_cart, coupon)
+
+  consolidated_cart.each {|cart_item|
+    if cart_item[:item] == coupon[:item]
+      if cart_item[:count] >= coupon[:num]
+        consolidated_cart << {
+          :item => "#{cart_item[:item]} W/COUPON",
+          :price => coupon[:cost]/coupon[:num],
+          :clearance => cart_item[:clearance],
+          :count => coupon[:num]
+        }
+        cart_item[:count] = cart_item[:count] - coupon[:num]
+      end
+    end
+  }
+  consolidated_cart
+end
+
+
+def apply_coupons(consolidated_cart, coupons)
+
+  coupons.each {|coupon|
+  consolidated_cart = apply_coupon_to_cart(consolidated_cart, coupon)
+  }
+
+  consolidated_cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+
+cart.each {|item|
+   if item[:clearance] == true
+      item[:price] = (item[:price] * 0.8).round(2)
+   end
+}
+cart
 end
+
 
 def checkout(cart, coupons)
   # Consult README for inputs and outputs
@@ -33,4 +96,20 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+
+consolidated_cart = consolidate_cart(cart)
+consolidated_cart = apply_coupons(consolidated_cart, coupons)
+consolidated_cart = apply_clearance(consolidated_cart)
+
+total = 0.0
+
+consolidated_cart.each {|cart_item|
+  total += cart_item[:price] * cart_item[:count]
+}
+
+if total > 100
+  total * 0.9
+else total
+end
+
 end
